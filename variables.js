2204571c1d0ccc6a6c272180bc39a623fa4aa6c2
kamalpratap87 <kamalpratap.87@gmail.com>
2023-09-09 16:19:18 +0530

    Commit datatypes

--- variables.js
+++ variables.js
@@ -2,13 +2,14 @@ const accountId=654321
 let accountEmail="kamalpratap.87@gmail.com"
 var accountPassword="123456"
 accountCity="Noida"
+let accountState
 
 
 //accountId=2
 
 console.log(accountId)
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
 /*
 Use let in place of var
