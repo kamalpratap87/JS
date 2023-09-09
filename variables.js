5baed78fb6e37d4adbfae87e63e5e2f914bcac2c
kamalpratap87 <kamalpratap.87@gmail.com>
2023-09-09 15:58:09 +0530

    Commit Variables.js

---
+++ variables.js
@@ -0,0 +1,17 @@
+const accountId=654321
+let accountEmail="kamalpratap.87@gmail.com"
+var accountPassword="123456"
+accountCity="Noida"
+
+
+//accountId=2
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+Use let in place of var
+
+*/
+
